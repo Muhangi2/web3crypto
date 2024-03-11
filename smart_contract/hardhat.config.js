2677b0a9d899ae512a7480bb5c972cf3e4f54140
Muhangi2 <eliodamutiba@gmail.com>
2024-03-11 23:38:06 +0300

    lates

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox");
+// https://eth-sepolia.g.alchemy.com/v2/Lg05zvpJ9NWpPP8pzjJs6Zkeo9kISVXU
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-waffle");
+
 module.exports = {
-  solidity: "0.8.24",
+  solidity: "0.8.0",
+  networks: {
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/Lg05zvpJ9NWpPP8pzjJs6Zkeo9kISVXU",
+      account: [process.env.PRIVATE_KEY],
+    },
+  },
 };
